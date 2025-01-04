0202a20618c1efd2da9dd2a1fe06ae447841469d
Macharete_r <rafaelmacharete00@gmail.com>
2025-01-03 22:46:21 -0300

    03.01.2025.22:46

---
+++ react-initial-project/src/data/peopleList.ts
@@ -0,0 +1,11 @@
+import { Person } from "@/types/Person";
+
+export const peopleList: Person[] = [
+    { id: 1, name: 'Rafael', profession: 'PO'},
+    { id: 2, name: 'Leafar', profession: 'Aprendiz' },
+    { id: 3, name: 'Lafear', profession: 'TechLead'},
+    { id: 4, name: 'Farael', profession: 'PO' },
+    { id: 5, name: 'Raelaf', profession: 'GP' },
+    { id: 6, name: 'Lafear', profession: 'TechLead'},
+    { id: 7, name: 'Lafear', profession: 'PO' }
+];
